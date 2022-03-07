27827d7beed6ce494a642de0858ce1945cdd4247
Julius Alfredo <ju.27.al@gmail.com>
2022-03-07 13:21:04 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'JuliusAlfredo_36372_IF670_AL_UTS',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
